d93ccd7e14f43b570e3d4a71fe5f1080a5894711
Jie Chen <jiechenmc@gmail.com>
2023-11-28 14:57:10 -0500

    cp

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char const *argv[])
 {
-    printf("HELLO WORLD\n");
+    char *buffer = NULL;
+    size_t bufsize = 0;
+
+    getline(&buffer, &bufsize, stdin);
+
+    printf("%s\n", buffer);
+
+    free(buffer);
     return 0;
 }
--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os/exec"
+)
 
 func main() {
-	fmt.Println("HELLO WORLD")
+	cmd := exec.Command("./a.out")
+	cmd.StdinPipe()
+	cmd.StdoutPipe()
+	err := cmd.Run()
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
