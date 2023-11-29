edc485ff4332b3d2297c7d0029e0131747ef6642
Jie Chen <jiechenmc@gmail.com>
2023-11-28 20:25:28 -0500

    cp

--- main.c
+++ main.c
@@ -1,14 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char const *argv[])
 {
     char *buffer = NULL;
     size_t bufsize = 0;
 
-    getline(&buffer, &bufsize, stdin);
+    int managed_arr[5] = {0};
+    int managed_arr_ptr = 0;
 
-    printf("%s\n", buffer);
+    // SIMPLE MANAGER TO ADD SOMETHING TO AN ARRAY
+    while (getline(&buffer, &bufsize, stdin) != EOF)
+    {
+        if (!strcmp(buffer, "ADD\n"))
+        {
+            fprintf(stderr, "ADD COMMAND RECV\n");
+            managed_arr[managed_arr_ptr++] = 1;
+        }
+        else if (!strcmp(buffer, "REMOVE\n"))
+        {
+            fprintf(stderr, "REMOVE COMMAND RECV\n");
+            managed_arr[managed_arr_ptr--] = 0;
+        }
+        else if (!strcmp(buffer, "SHOW\n"))
+        {
+            fprintf(stderr, "REMOVE COMMAND RECV\n");
+            for (int i = 0; i < 5; i++)
+            {
+                printf("%d |", managed_arr[i]);
+            }
+            printf("\n");
+        }
+    }
 
     free(buffer);
     return 0;
--- main.go
+++ main.go
@@ -1,17 +1,53 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"os/exec"
+	"strings"
 )
 
 func main() {
 	cmd := exec.Command("./a.out")
-	cmd.StdinPipe()
-	cmd.StdoutPipe()
-	err := cmd.Run()
+	stdin, e := cmd.StdinPipe()
 
-	if err != nil {
-		log.Fatal(err)
+	if e != nil {
+		log.Fatal(e)
+	}
+	stdout, e := cmd.StdoutPipe()
+
+	if e != nil {
+		log.Fatal(e)
+	}
+
+	if e := cmd.Start(); e != nil {
+		log.Fatal(e)
+	}
+	_, e = stdin.Write([]byte("ADD\n"))
+	if e != nil {
+		log.Fatal(e)
+	}
+
+	_, e = stdin.Write([]byte("ADD\n"))
+	if e != nil {
+		log.Fatal(e)
+	}
+
+	_, e = stdin.Write([]byte("SHOW\n"))
+	_, e = stdin.Write([]byte("ADD\n"))
+	if e != nil {
+		log.Fatal(e)
+	}
+	_, e = stdin.Write([]byte("SHOW\n"))
+	stdin.Close()
+
+	out, _ := io.ReadAll(stdout)
+
+	fmt.Println(strings.TrimRight(string(out), "\n"))
+	fmt.Println(strings.TrimRight(string(out), "\n") == "hello\nworld")
+
+	if e := cmd.Wait(); e != nil {
+		log.Fatal(e)
 	}
 }
